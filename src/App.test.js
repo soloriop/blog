d907a0c1628189442b7748de16f51cdd2f141fa6
Carlos Solorio <s.carlosalexander02@gmail.com>
2026-01-15 22:17:56 -0600

    hasta que quedo vlv

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import MainContent from './components/MainContent';
-
-test('renders learn react link', () => {
-  render(<MainContent />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
